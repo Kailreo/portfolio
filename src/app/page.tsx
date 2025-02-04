fe121cf898334226cac5d33f6f282ab9e80377f6
Kylar <kylarry2009@gmail.com>
2025-02-04 17:22:30 -0500

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>testing</h1>
+        <h1>kylar's portfolio site</h1>
         <Image
           className={styles.logos}
           src="/next.svg"
